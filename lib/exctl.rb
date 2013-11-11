bd1de336705c9aef2c6db268b2e48441610e0aff
Joseph Wecker <joseph.wecker@gmail.com>
2013-11-11 15:17:04 -0800

    Help & version commands working fine. quick skel for init and ready for more.

--- lib/exctl.rb
+++ lib/exctl.rb
@@ -1,9 +1,7 @@
-require 'exctl/cli'
-
 module Exctl
   VERSION = File.exist?(File.join(File.dirname(__FILE__),'..','VERSION')) ? File.read(File.join(File.dirname(__FILE__),'..','VERSION')) : ""
   def self.version() Exctl::VERSION end
-  def self.cli(argv)
-    Exctl::CLI.new(argv).run
-  end
 end
+
+require 'exctl/cli'
+
