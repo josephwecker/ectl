d7d8fa853195cf820b73b5a309bae29c13e39d8e
Joseph Wecker <joseph.wecker@gmail.com>
2013-11-15 19:00:10 -0800

    Starting to parse the command manifests

--- lib/exctl/commands.rb
+++ lib/exctl/commands.rb
@@ -3,7 +3,9 @@ module Exctl
     include Enumerable
     def initialize(proj_root)
       @root = proj_root
-      @cmd_files = Path[@root]['**/**/.commands']
+      @cmd_manifests = Path[@root]['**/**/.commands']
+      #@bin_files = `find '#{@root}' -executable -type f -print0`.split("\x00")
+      #pp @bin_files
     end
 
     def each(&block) commands.each(&block) end
@@ -15,10 +17,16 @@ module Exctl
       # 2. Default commands (unless overridden already)
       # 3. Commands found in 'scripts' etc. (unless overridden)
 
-      @cmd_files.each do
-        # TODO: You are here- figure out what representation they should be in, pull them in and eval/parse/etc. them
+      @commands = []
+      @cmd_manifests.each do |cf|
+        cmd_path = cf.short(Path[@root]).to_s.split('/')[0..-2]
+        @commands << cmd_path
+        eval(File.read(cf), binding, cf.to_s)
       end
+      @commands
+    end
 
+    def 
 
     end
   end
--- lib/exctl/dispatch.rb
+++ lib/exctl/dispatch.rb
@@ -4,6 +4,8 @@ module Exctl
   class Dispatch
     def self.dispatch(proj_root, args)
       commands = Exctl::Commands.new(proj_root)
+
+      pp commands.to_a
     end
   end
 end
